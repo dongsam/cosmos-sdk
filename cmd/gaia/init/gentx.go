5aa9778bf7a977194f4b2f5541b9946dd891b214
Alessio Treglia <alessio@tendermint.com>
2018-11-22 01:29:25 +0000

    Don't overwrite output file if it exists already.

--- cmd/gaia/init/gentx.go
+++ cmd/gaia/init/gentx.go
@@ -177,7 +177,7 @@ func readUnsignedGentxFile(cdc *codec.Codec, r io.Reader) (auth.StdTx, error) {
 }
 
 func writeSignedGenTx(cdc *codec.Codec, outputDocument string, tx auth.StdTx) error {
-	outputFile, err := os.OpenFile(outputDocument, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+	outputFile, err := os.OpenFile(outputDocument, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0644)
 	if err != nil {
 		return err
 	}
